6f9041704a6c7d18b7a3db9b183107092f505ce7
Rohamsh <roham.sharifi44@gmail.com>
2025-04-17 20:44:50 +0330

    VerificationCodes table created!

--- server/server.js
+++ server/server.js
@@ -37,6 +37,38 @@ const User = sequelize.define("User", {
     }
 );
 
+User.sync({ alter: true });
+
+const VerificationCode = sequelize.define("VerificationCode", {
+    code_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    phone_number: {
+        type: DataTypes.STRING(11),
+        allowNull: false,
+        unique: true
+    },
+    code: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    expiresAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: () => new Date(Date.now() + 90 * 1000)
+    }
+},
+    {
+        timestamps: false
+    }
+);
+
+VerificationCode.sync({ alter: true });
+
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -46,15 +78,16 @@ app.use(cors({
 }));
 app.use(express.json());
 
-let verificationCodes = {};
 
 app.post('/login', async (req, res) => {
     const { phone_number } = req.body;
-    const verificationCode = Math.floor(10000 + Math.random() * 90000);
-    verificationCodes[phone_number] = verificationCode;
+    const verificationCode = Math.floor(10000 + Math.random() * 90000).toString();
+    await VerificationCode.create({
+        phone_number: phone_number,
+        code: verificationCode,
+    });
     return res.status(200).json({ message: 'Verification code sent', code: verificationCode });
 });
-
 
 
 app.post('/login/verification', async (req, res) => {
@@ -62,15 +95,18 @@ app.post('/login/verification', async (req, res) => {
     const user = await User.findOne({ where: { phone_number } });
 
     if (!user) {
-        const newUser = await User.create({ phone_number });
+        await User.create({ phone_number });
     }
 
-    const storedCode = verificationCodes[phone_number];
+    const record = await VerificationCode.findOne({ where: { phone_number } });
+    const storedCode = record.code;
+    if (!storedCode) {
+        res.status(400).json({ message: 'Verification code not exist!' });
+    }
 
     if (code === storedCode.toString()) {
         // const token = jwt.sign({ phone_number }, process.env.JWT_SECRET, { expiresIn: '1h' });
         res.status(200).json({ message: 'Verified successfully' });
-
     }
     else {
         res.status(400).json({ message: 'Invalid verification code' });
