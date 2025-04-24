e77ba8d654a117d5367835c03c3471e662c7a092
Rohamsh <roham.sharifi44@gmail.com>
2025-04-24 13:42:59 +0330

    Shopping cart and profile UI fixed!

--- client/src/Components/Commons/Navbar/navbar.jsx
+++ client/src/Components/Commons/Navbar/navbar.jsx
@@ -1,15 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { CiShoppingCart } from "react-icons/ci";
+import { RiArrowDropDownFill } from "react-icons/ri"
+import { CiShoppingBasket } from "react-icons/ci"
+import { CiUser } from "react-icons/ci";
+import { CiLogout } from "react-icons/ci";
 import { Link } from "react-router-dom";
 
 import "./navbar.css";
 
 const Navbar = ({ handleOpenSidenav }) => {
-  let [buttonDisplay, setButtinDisplay] = useState("inline");
+  let [buttonDisplay, setButtonDisplay] = useState("inline");
+  let [userIconDisplay, setUserIconDisplay] = useState("flex");
   let [divWidth, setDivWidth] = useState("200px");
+  let [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const userMenuRef = useRef(null);
 
   useEffect(() => {
     const token = localStorage.getItem('token');
@@ -17,14 +25,29 @@ const Navbar = ({ handleOpenSidenav }) => {
   }, []);
 
   const handleFocus = () => {
-    setButtinDisplay("none");
+    setButtonDisplay("none");
+    setUserIconDisplay("none")
     setDivWidth("100%");
   };
 
   const handleBlur = ({ }) => {
     setDivWidth("200px");
-    setTimeout(() => setButtinDisplay("inline"), 540);
+    setTimeout(() => setButtonDisplay("inline"), 540);
+    setTimeout(() => setUserIconDisplay("flex"), 540);
   };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (userMenuRef.current && !userMenuRef.current.contains(event.target)) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <nav>
@@ -49,19 +72,45 @@ const Navbar = ({ handleOpenSidenav }) => {
             />
           </div>
         </div>
-        {false ? (
-          <Link to="/profile" style={{ textDecoration: "none" }}>
-            <button className="signin-button" style={{ display: buttonDisplay }}>
-              پروفایل
-            </button>
+        <div className="cart-section">
+          {isLoggedIn ? (
+            <div
+              className={`profile-section ${isMenuOpen ? "active" : ""}`}
+              onClick={() => setIsMenuOpen(prev => !prev)}
+              ref={userMenuRef}
+            >
+              <div className="user-icon-container" style={{ display: userIconDisplay }}>
+                <CiUser className="user-icon" />
+                <RiArrowDropDownFill className={`user-icon-arrow ${isMenuOpen ? "active" : ""}`} />
+              </div>
+              {isMenuOpen && (
+                <ul className="user-dropdown-menu">
+                  <li>
+                    <Link to="orders" className="user-menu-link">
+                      <CiShoppingBasket className="user-menu-icons" />
+                      سفارش‌ها
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="exit" className="user-menu-link">
+                      <CiLogout className="user-menu-icons" />
+                      خروج از حساب کاربری
+                    </Link>
+                  </li>
+                </ul>
+              )}
+            </div>
+          ) : (
+            <Link to="/login" style={{ textDecoration: "none" }}>
+              <button className="signin-button" style={{ display: buttonDisplay }}>
+                ورود یا ثبت‌نام
+              </button>
+            </Link>
+          )}
+          <Link to="/shopping-cart" className="shopping-cart-link">
+            <CiShoppingCart className="shopping-cart" />
           </Link>
-        ) : (
-          <Link to="/login" style={{ textDecoration: "none" }}>
-            <button className="signin-button" style={{ display: buttonDisplay }}>
-              ورود یا ثبت‌نام
-            </button>
-          </Link>
-        )}
+        </div>
       </section>
       <div className="menu">
         <ul className="menu-list">
