44a0b04dee57ef46759b2a069e7f1350a1cd6969
Rohamsh <roham.sharifi44@gmail.com>
2025-06-17 12:32:38 +0330

    Main category page updated.

---
+++ client/src/Components/Commons/Categories/filterMenu.jsx
@@ -0,0 +1,12 @@
+import './filterMenu.css';
+
+const FilterMenu = ({ filterMenuClass }) => {
+
+    return (
+        <aside className={filterMenuClass}>
+            hello
+        </aside>
+    );
+};
+
+export default FilterMenu;
--- client/src/Components/Commons/Categories/mainCategory.jsx
+++ client/src/Components/Commons/Categories/mainCategory.jsx
@@ -1,19 +1,64 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
 import Navbar from '../Navbar/navbar';
 import Header from '../Header/header';
+import FilterMenu from "./filterMenu";
 
 import './paintingTools.css';
 import { FaChevronDown } from "react-icons/fa";
 
 const MainCategory = () => {
     const { maincategory } = useParams();
-    console.log(maincategory);
 
     const [subCategories, setSubCategories] = useState([]);
     let [isCategoryActive, setIsCategoryActive] = useState(false);
+    let [isOrderActive, setIsOrderActive] = useState(false);
+    let [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const scMenuRef = useRef(null);
+    const orderMenuRef = useRef(null);
+    const filterMenuRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (scMenuRef.current && !scMenuRef.current.contains(event.target)) {
+                setIsCategoryActive(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (orderMenuRef.current && !orderMenuRef.current.contains(event.target)) {
+                setIsOrderActive(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (filterMenuRef.current && !filterMenuRef.current.contains(event.target)) {
+                setIsMenuOpen(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
 
     useEffect(() => {
         axios.get('http://localhost:5000/api/subcategories')
@@ -51,50 +96,81 @@ const MainCategory = () => {
     });
 
     const mainCategories = {
-        paintingTools: toolSubCategories,
-        carPaint: carSubCategories,
-        woodPaint: woodSubCategories,
-        housePaint: houseSubCategories,
-        industrialPaint: industrialSubCategories
+        paintingTools: [toolSubCategories, 'ابزارها'],
+        carPaint: [carSubCategories, 'رنگ اتومبیلی'],
+        woodPaint: [woodSubCategories, 'رنگ چوب'],
+        housePaint: [houseSubCategories, 'رنگ ساختمانی'],
+        industrialPaint: [industrialSubCategories, 'رنگ صنعتی']
     };
 
-    const subCategory = mainCategories[maincategory];
+    const data = mainCategories[maincategory];
+
+    const orders = ['ارزان ترین', 'گران ترین', 'پرفروش ترین'];
 
     let iconClassname = `sc-dropdown-icon ${isCategoryActive ? 'active' : null}`;
-    let iconClassnameOrder = `sc-dropdown-icon-order ${isCategoryActive ? 'active' : null}`;
+    let subcategoryDivClassname = `subcategory-div ${isCategoryActive ? 'active' : null}`;
+    let iconClassnameOrder = `sc-dropdown-icon-order ${isOrderActive ? 'active' : null}`;
     let ulClassname = `tools-subcategories-ul ${isCategoryActive ? 'active' : null}`;
+    let orderUlClassname = `tools-subcategories-ul order ${isOrderActive ? 'active' : null}`;
+    let byOrderDivClassname = `byorder-div ${isOrderActive ? 'active' : null}`;
+    let filterMenuClass = `filter-menu-aside ${isMenuOpen ? 'open' : null}`;
+    let opacity = isMenuOpen ? '0.2' : '1';
 
     return (
-        <div>
-            <Header />
-            <Navbar />
-            <div className="tools-container">
-                <div className="tools-top-menu">
-                    <div onClick={() => setIsCategoryActive(!isCategoryActive)}>
-                        <button
-                            className="tools-subcategories-button"
+        <main>
+            <FilterMenu filterMenuClass={filterMenuClass} />
+            <div style={{ opacity: opacity }}>
+                <Header />
+                <Navbar />
+                <div className="tools-container">
+                    <div className="tools-top-menu">
+                        <div
+                            onClick={() => setIsCategoryActive(!isCategoryActive)}
+                            ref={scMenuRef}
+                            className={subcategoryDivClassname}
                         >
-                            دسته‌بندی ابزارها
-                        </button>
-                        <FaChevronDown className={iconClassname} />
-                    </div>
-                    <ul className={ulClassname}>
-                        {subCategory.map((s) => {
-                            return (
-                                <li key={s.ScId} className="tools-subcategories-li">
-                                    <div>{s.ScName}</div>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                    <button>فیلترها</button>
-                    <div>
-                        <button className="tools-byorder-button">ترتیب نمایش</button>
-                        <FaChevronDown className={iconClassnameOrder} />
+                            <button className="tools-subcategories-button">
+                                دسته‌بندی {data[1]}
+                            </button>
+                            <FaChevronDown className={iconClassname} />
+                        </div>
+                        <ul className={ulClassname}>
+                            {data[0].map((s) => {
+                                return (
+                                    <li key={s.ScId} className="tools-subcategories-li">
+                                        <div>{s.ScName}</div>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                        <button
+                            className="maincategory-filters"
+                            onClick={() => setIsMenuOpen(true)}
+                            ref={filterMenuRef}
+                        >فیلترها</button>
+                        <div>
+                            <div
+                                className={byOrderDivClassname}
+                                onClick={() => setIsOrderActive(!isOrderActive)}
+                                ref={orderMenuRef}
+                            >
+                                <button className="tools-byorder-button">ترتیب نمایش</button>
+                                <FaChevronDown className={iconClassnameOrder} />
+                            </div>
+                            <ul className={orderUlClassname}>
+                                {orders.map((order, index) => {
+                                    return (
+                                        <li key={index} className="tools-subcategories-li">
+                                            <div>{order}</div>
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </main>
     );
 }
 export default MainCategory;
